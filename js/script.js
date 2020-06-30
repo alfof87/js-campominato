badf5027632ccd1c619c5ed8525d86471bd9a799
Alfonso <alfof87@gmail.com>
2020-06-30 17:25:06 +0200

    js-campominato prova vecchia

--- js/script.js
+++ js/script.js
@@ -7,38 +7,48 @@ for (var i = 0; i < 5; i++) {
   arrayPc.push(numeroCasualePc);
   console.log(arrayPc);
 }
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta,
-//  sempre compreso tra 1 e 100.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero
+//alla volta, sempre compreso tra 1 e 100.
+
 // L’utente non può inserire più volte lo stesso numero.
 
-var arrayUtente = [];
-for (var i = 0; i < 5; i++) {
-  var numeroUtente = parseInt(prompt("Inserisci un numero"));
-  arrayUtente.push(numeroUtente);
-  console.log(arrayUtente);
-}
- for (var c = 0; c < numeroCasualePc.length; c++) {
-   if (numeroUtente == numeroCasualePc) {
-     console.log("hai perso");
-   }else {
-     arrayUtente.push(numeroUtente);
-   }
- }
+// var arrayUtente = [];
+//
+// for (var i = 0; i < 5; i++) {
+//   var numeroUtente = parseInt(prompt("Inserisci un numero"));
+//   arrayUtente.push(numeroUtente);
+//   console.log(arrayUtente);
+// }
+//
+//  for (var c = 0; c < numeroCasualePc.length; c++) {
+//    if (numeroUtente == numeroCasualePc) {
+//      console.log("hai perso");
+//    }else {
+//      arrayUtente.push(numeroUtente);
+//    }
+//  }
 
 // Se il numero è presente nella lista dei numeri generati, la partita termina,
 // altrimenti si continua chiedendo all’utente un altro numero.
 
-// var arrayAltroNumero =[];
-// var i = 0;
-// if (arrayUtente[i] == arrayPc[i]) {
-//   console.log("Hai perso");
-// }else {
-//   for (var i = 0; i < 10; i++) {
-//     var altroNumero = prompt("Inserisci un altro numero");
-//     arrayAltroNumero.push(altroNumero);
-//     console.log(arrayAltroNumero);
-//   }
-// }
+// PROVA VECCHIA:
+var arrayAltroNumero =[];
+var arrayUtente = [];
+var c = 0;
+if (arrayUtente[c] == arrayPc[c]) {
+  console.log("Hai perso");
+}else {
+  for (var i = 0; i < 10; i++) {
+    var altroNumero = parseInt(prompt("Inserisci un altro numero"));
+    arrayAltroNumero.push(altroNumero);
+    console.log(arrayAltroNumero);
+  }
+  if (arrayAltroNumero[i] == arrayPc[i]) {
+    console.log("Hai perso");
+  }else{
+    console.log("hai vinto");
+  }
+}
 
 
 
